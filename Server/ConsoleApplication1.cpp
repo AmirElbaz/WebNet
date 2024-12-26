7564f37942b4f279b47e8a3102ac5e7db766e65d
AmirElbaz <m_sh8004@yahoo.com>
2024-12-26 20:33:05 +0200

    setup server socket

--- Server/ConsoleApplication1.cpp
+++ Server/ConsoleApplication1.cpp
@@ -2,19 +2,73 @@
 //
 
 #include <iostream>
+#include "stdafx.h"
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "\n=== step 1 - set the dll ===" << endl;
+	SOCKET serverSocket, acceptSocket;
+	int port = 55555; //server default port number
+	WSADATA wsaData; // windows socket api data object sent as referense to collect data about the connection
+	int wsaerr;
+	WORD wVersionRequested = MAKEWORD(2, 2); // parsing the version 2.2 to 16 bits word and generate the versioning word expected by WSAStartup
+	wsaerr = WSAStartup(wVersionRequested, &wsaData); // initialize the winsock
+	if (wsaerr != 0) {
+		cout << "The winsock dll was not found!" << endl;
+		return 0;
+	}
+	else {
+		cout << "the winsock dll was found" << endl;
+		cout << "status: " << wsaData.szSystemStatus << endl;
+	}
+
+	cout << "\n=== step 2 - set up server socket ===" << endl;
+	serverSocket = INVALID_SOCKET;
+	serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); // bind the winsock to a TCP protocol
+	if (serverSocket == INVALID_SOCKET) {
+		cout << "Error at socket" << WSAGetLastError() << endl;
+		WSACleanup();
+		return 0;
+	}
+	else {
+		cout << "socket is ok" << endl;
+	}
+
+	cout << "\n=== step -3 bind the socket to the ip and port number ===" << endl;
+	sockaddr_in service;
+	service.sin_family = AF_INET;
+	InetPton(AF_INET, _T("127.0.0.1"), &service.sin_addr.s_addr);
+	service.sin_port = htons(port);
+	if (bind(serverSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
+		cout << "Binding failed" << endl;
+		closesocket(serverSocket);
+		WSACleanup();
+		return 0;
+	}
+	else {
+		cout << "bind is ok" << endl;
+	};
+
+	cout << "\n=== step -4 initiate listen ===" << endl;
+	if (listen(serverSocket, 1) == SOCKET_ERROR)
+		cout << "listen() error listening in socket" << endl;
+	else
+		cout << "listening is OK, waiting for connections...." << endl;
+	cout << "\n=== step -5 Accept connection ===" << endl;
+
+	acceptSocket = accept(serverSocket, NULL, NULL);
+	if (acceptSocket == INVALID_SOCKET) {
+		cout << "Accept failed" << endl;
+		WSACleanup();
+		return -1;
+	}
+	else {
+		cout << "Accept connection" << endl;
+		system("pause");
+		WSACleanup();
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
