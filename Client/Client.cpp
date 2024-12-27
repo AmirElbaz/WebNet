dbc990a0a5fb7d911857dc010da67889be097a9c
AmirElbaz <m_sh8004@yahoo.com>
2024-12-27 03:13:32 +0200

    setup client socket

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,20 +1,58 @@
-// Client.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include "stdafx.h"
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "\n=== step 1 - set the dll ===" << endl;
+	SOCKET clientSocket;
+	int port = 55555; //server default port number
+	WSADATA wsaData; // windows socket api data object sent as referense to collect data about the connection
+	int wsaerr;
+	WORD wVersionRequested = MAKEWORD(2, 2); // parsing the version 2.2 to 16 bits word and generate the versioning word expected by WSAStartup
+	wsaerr = WSAStartup(wVersionRequested, &wsaData); // initialize the winsock
+	if (wsaerr != 0) {
+		cout << "The winsock dll was not found!" << endl;
+		return 0;
+	}
+	else {
+		cout << "the winsock dll was found" << endl;
+		cout << "status: " << wsaData.szSystemStatus << endl;
+	}
+
+	cout << "\n=== step 2 - set up server socket ===" << endl;
+
+	clientSocket = INVALID_SOCKET;
+	clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); // bind the winsock to a TCP protocol
+	if (clientSocket == INVALID_SOCKET) {
+		cout << "Error at socket" << WSAGetLastError() << endl;
+		WSACleanup();
+		return 0;
+	}
+	else {
+		cout << "socket is ok" << endl;
+	}
+
+	cout << "\n=== step 3 - Connect with server ===" << endl;
+	sockaddr_in clientService;
+	clientService.sin_family = AF_INET;
+	InetPton(AF_INET, _T("127.0.0.1"), &clientService.sin_addr.s_addr);
+	clientService.sin_port = htons(port);
+	if (connect(clientSocket, (SOCKADDR*)&clientService, sizeof(clientService)) == SOCKET_ERROR) {
+		cout << "\nclient connect() failed to connect" << endl;
+		WSACleanup();
+		return 0;
+
+	}
+	else {
+		cout << "client connect() is OK" << endl;
+		cout << "client can start sending and recieving data..." << endl;
+
+	}
+	system("pause");
+	WSACleanup();
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
